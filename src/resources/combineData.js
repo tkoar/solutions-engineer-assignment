9849480d1a4ad92fe6a5a31e7399dfab605879a4
tkoar <terrancekoar@gmail.com>
2018-07-18 10:24:34 -0400

    added comments

--- src/resources/combineData.js
+++ src/resources/combineData.js
@@ -4,7 +4,7 @@ const restaurantsInfoJson = csvToJSON()
 const combinedRestaurantJSON = combineData(restaurantsInfoJson, restaurantsListJson)
 
 function csvToJSON(file='src/resources/dataset/restaurants_info.csv') {
-  let text = FS.readFileSync(file, 'utf8', (err, data) => {
+  const text = FS.readFileSync(file, 'utf8', (err, data) => {
     if (err) {
       console.log(err)
       console.log(`Failed to read ${file}`);
@@ -16,12 +16,13 @@ function csvToJSON(file='src/resources/dataset/restaurants_info.csv') {
 }
 
 function convertToJson(csvArray) {
-  const headers = csvArray[0].split(";") // create array with the value for each header, to use as keys
-  const restaurantDataArray = csvArray.slice(1) // create separate array for data and remove headers
-  let json = restaurantDataArray.map((restaurantInfo, idx) => {
-    return createJsonObj(headers, restaurantInfo)
-  })
-  return json
+  // create array with the value for each header, to use as keys
+  const headers = csvArray[0].split(";")
+  // create separate array for data and remove headers
+  const restaurantDataArray = csvArray.slice(1)
+  // returns an array of JSON objects with the data for each restaurant
+  // using the headers as keys and the data for an individual restaurant to create a JSON object
+  return restaurantDataArray.map((restaurantInfo, idx) => createJsonObj(headers, restaurantInfo))
 }
 
 function createJsonObj(headers, restuarantDataString) {
@@ -39,14 +40,15 @@ function createJsonObj(headers, restuarantDataString) {
 }
 
 function cleanData(dataArray) {
+  // removing any undefined elements in the array of data
   return dataArray.filter((dataElement, idx) => !!dataElement)
 }
 
 function combineData(restaurantInfo, restaurantList) {
-  let sortedRestaurantInfo = restaurantInfo.sort((a, b) => {
+  const sortedRestaurantInfo = restaurantInfo.sort((a, b) => {
     return parseInt(a.objectID) - parseInt(b.objectID)
   })
-  let sortedRestaurantList = restaurantList.sort((restaurant1, restaurant2) => {
+  const sortedRestaurantList = restaurantList.sort((restaurant1, restaurant2) => {
     return parseInt(restaurant1.objectID) - parseInt(restaurant2.objectID)
   })
   return sortedRestaurantInfo.map((restaurant, idx) => {
@@ -57,7 +59,7 @@ function combineData(restaurantInfo, restaurantList) {
 // once the data from the CSV is parsed into JSON and combined with the existing JSON data
 // we write the combined JSON to a new JSON file to use as our index's dataset
 FS.writeFile('src/resources/dataset/combined_restaurant_data_fixed.json', JSON.stringify(combinedRestaurantJSON), 'utf8', (err) => {
-  if(err) {
+  if (err) {
     console.log(err)
   }
 })
---
+++ src/resources/indexData.js
@@ -0,0 +1,21 @@
+const algoliasearch = require('algoliasearch');
+const algoliasearchHelper = require('algoliasearch-helper');
+const combinedRestaurantJSON = require('./dataset/combined_restaurant_data_fixed.json')
+const APP_ID = 'ZPSJDZC155'
+const indexName = 'open_table_restaurants'
+
+// function indexData(jsonData, appId, indexName) {
+//   fetch('https://restaurant-locator-server.herokuapp.com/auth/generate')
+//     .then(resp => resp.json())
+//     .then(json => {
+//       const client = algoliasearch(appId, json['secure_key'])
+//       const index = client.initIndex(indexName)
+//       index.addObjects(jsonData, (err, content) => {
+//         if(err) {
+//           console.log(err, "Content Not Added")
+//         } else {
+//           console.log(content,"Contents added to index successfully!")
+//         }
+//       })
+//     })
+// }
